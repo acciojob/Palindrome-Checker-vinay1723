645abab4c198f5d8aa5898fe3b7daf67c5b63561
Vinay Kumar Goud <144577819+vinay1723@users.noreply.github.com>
2026-02-04 15:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-
+ str = str.replace(/[^a-zA-Z0-9]/g,"");
+	return str === str.split("").reverse().join("");
 }
 module.exports = palindrome
